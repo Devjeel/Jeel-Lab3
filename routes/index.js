5d7c963c84db028cea75a6ec49f6218b66a7edfc
devjeel <jeelhp02@gmail.com>
2019-10-01 20:25:06 -0400

    Add project files.

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/bhavya', function (req, res) {
+    res.render('bhavya', { title: 'Bhavya' });
+});
+
+router.get('/jeel', function (req, res) {
+    res.render('Jeel', { title: 'Jeel' });
+});
+
+router.get('/hits', function (req, res) {
+    res.render('Hits', { title: 'Hits' });
+});
+
+router.get('/pina', function (req, res) {
+    res.render('Pina', { title: 'Pina' });
+});
+
+module.exports = router;
